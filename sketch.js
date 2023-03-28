389d614c6b65a2c1851bb0b4fd799003cd26fd75
fevan35 <122712198+fevan35@users.noreply.github.com>
2023-03-28 20:21:52 +0000

    Updatedd sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
+class Rock {
+  constructor(x, altitude, speed) {
+    this.x = x;
+    this.altitude = altitude;
+    this.speed = speed;
+  }
+
+  update() {
+    this.altitude -= this.speed;
+    if(this.altitude <= 4){
+      this.altitude = 4;
+    }
+  }
+
+  draw() {
+    ellipse(this.x, height - this.altitude, 10);
+    this.update();
+  }
+}
+
+class ParaRock extends Rock {
+  constructor(x, altitude, speed){
+    super(x, altitude, speed);
+  }
+  
+  draw() {
+    ellipse(this.x, height - this.altitude - 10, 30);
+    ellipse(this.x, height - this.altitude, 10);
+    text('overload', this.x - 15, height - this.altitude - 15);
+    this.update();
+  }
+}
+
+let myRock;
+let leftRock;
+
 function setup () {
-  // put setup code here
+  createCanvas(300,300);
+  myRock = new Rock(width / 2, height, 1);
+  leftRock = new ParaRock(100, height, 0.8);
 }
 
 function draw () {
-  // put drawing code here
+  background(200);
+  myRock.draw();
+  leftRock.draw();
 }
